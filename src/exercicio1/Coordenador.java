2fd48d594a6d1c5e3f348b320d5d9c3dafe2852f
davispacheco <davipacheco1996@gmail.com>
2022-07-12 10:50:02 -0300

    Criar classe Professor herdada de Funcionario, com os atributos específicos nivelDeGraduacao, disciplinaMinistrada, quantidadeDeAlunos e quantidadeDeTurmas, bem como o método adicionaTurma.

---
+++ src/exercicio1/Coordenador.java
@@ -0,0 +1,2 @@
+package src.exercicio1;public class Coordenador {
+}
---
+++ src/exercicio1/Professor.java
@@ -0,0 +1,50 @@
+package src.exercicio1;
+
+public class Professor extends Funcionario {
+private String nivelDeGraduacao;
+private String disciplinaMinistrada;
+private int quantidadeDeAlunos;
+private int quantidadeDeTurmas;
+
+    public Professor() {
+        this.nivelDeGraduacao = nivelDeGraduacao;
+        this.disciplinaMinistrada = disciplinaMinistrada;
+        this.quantidadeDeAlunos = 0;
+        this.quantidadeDeTurmas = 0;
+    }
+
+    public String getNivelDeGraduacao() {
+        return nivelDeGraduacao;
+    }
+
+    public void setNivelDeGraduacao(String nivelDeGraduacao) {
+        this.nivelDeGraduacao = nivelDeGraduacao;
+    }
+
+    public String getDisciplinaMinistrada() {
+        return disciplinaMinistrada;
+    }
+
+    public void setDisciplinaMinistrada(String disciplinaMinistrada) {
+        this.disciplinaMinistrada = disciplinaMinistrada;
+    }
+
+    public int getQuantidadeDeAlunos() {
+        return quantidadeDeAlunos;
+    }
+
+    public void setQuantidadeDeAlunos(int quantidadeDeAlunos) {
+        this.quantidadeDeAlunos = quantidadeDeAlunos;
+    }
+
+    public int getQuantidadeDeTurmas() {
+        return quantidadeDeTurmas;
+    }
+
+    public void setQuantidadeDeTurmas(int quantidadeDeTurmas) {
+        this.quantidadeDeTurmas = quantidadeDeTurmas;
+    }
+    public void adicionaTurma() {
+        quantidadeDeTurmas ++;
+    }
+}

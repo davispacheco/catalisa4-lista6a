d47c3284ab6a59953d4a975b0070677e9afe35bf
davispacheco <davipacheco1996@gmail.com>
2022-07-14 16:29:03 -0300

    Alterar posição de getters and setters na classe FiguraGeometrica (pacote exercicio1).

--- src/lista6b/exercicio1/FiguraGeometrica.java
+++ src/lista6b/exercicio1/FiguraGeometrica.java
@@ -7,6 +7,8 @@ public abstract class FiguraGeometrica {
         this.areaFigura = areaFigura;
     }
 
+    public abstract void area();
+
     public double getAreaFigura() {
         return areaFigura;
     }
@@ -15,5 +17,5 @@ public abstract class FiguraGeometrica {
         this.areaFigura = areaFigura;
     }
 
-    public abstract void area();
+
 }
